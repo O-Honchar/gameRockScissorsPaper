e102a89e050d56b0ba6416243beda15c6a020a35
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-20 15:55:30 +0300

    feat: add html webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // 'use strict';
+import printMe from './print.js';
 
 const sum = (a, b) => a + b;
 console.log(sum(3, 5));
@@ -7,6 +8,12 @@ function component() {
   const element = document.createElement('div');
   const arr = ['Hello', 'webpack', '!'];
   element.innerHTML = arr.join(' ');
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   devServer: {
     contentBase: './dist',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Game-Rock-Scissors-Paper',
+    }),
+  ],
   output: {
-    filename: 'app.js',
+    // filename: 'app.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
