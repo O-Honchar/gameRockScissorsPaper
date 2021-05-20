0326a637faccbcb908a992c6fe4a5a3efd47bd0e
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-20 14:08:38 +0300

    delete: delete loaders and files for images, css, fonts... for next guide

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 // 'use strict';
-import './style.css';
-import Icon from './icon.png';
 
 const sum = (a, b) => a + b;
 console.log(sum(3, 5));
@@ -9,10 +7,6 @@ function component() {
   const element = document.createElement('div');
   const arr = ['Hello', 'webpack', '!'];
   element.innerHTML = arr.join(' ');
-  element.classList.add('hello');
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,4 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
 };
