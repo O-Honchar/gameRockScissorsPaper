1907cc00dab7a9195d44137b1f3f749ed90ca5af
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-20 13:23:13 +0300

    feat: add css file to check css loader, delete unused js from dist

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-console.log(8),document.body.appendChild(function(){const e=document.createElement("div");return e.innerHTML=["Hello","webpack"].join(" "),e}());
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // 'use strict';
+import './style.css';
 
 const sum = (a, b) => a + b;
 console.log(sum(3, 5));
@@ -7,6 +8,7 @@ function component() {
   const element = document.createElement('div');
   const arr = ['Hello', 'webpack', '!'];
   element.innerHTML = arr.join(' ');
+  element.classList.add('hello');
   return element;
 }
 
