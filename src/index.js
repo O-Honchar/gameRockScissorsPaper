6dc8b9ed150621dca295b9d87dda1761a3d09422
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-20 13:35:40 +0300

    feat: add rules for images, add test images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // 'use strict';
 import './style.css';
+import Icon from './icon.png';
 
 const sum = (a, b) => a + b;
 console.log(sum(3, 5));
@@ -9,6 +10,9 @@ function component() {
   const arr = ['Hello', 'webpack', '!'];
   element.innerHTML = arr.join(' ');
   element.classList.add('hello');
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
   return element;
 }
 
