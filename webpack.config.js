c7b6116a8d2cbe77f2a5aae31149ae3aa4db24d9
Oleh Honchar <O.O.Honchar@gmail.com>
2021-05-22 18:11:59 +0300

    feat: install eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -27,6 +28,9 @@ module.exports = {
             filename: 'index.html',
         }),
         new MiniCssExtractPlugin(),
+        new ESLintPlugin({
+            failOnError: true,
+        }),
     ],
 
     module: {
